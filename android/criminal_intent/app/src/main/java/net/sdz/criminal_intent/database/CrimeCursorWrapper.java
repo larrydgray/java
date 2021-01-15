6512ae6e7e5edc209ae4ac0743d7383e8e40e2cb
caverdude <javacaverdude@gmail.com>
2021-01-15 01:53:11 -0600

    ch14 p288 sqlite

---
+++ android/criminal_intent/app/src/main/java/net/sdz/criminal_intent/database/CrimeCursorWrapper.java
@@ -0,0 +1,31 @@
+package net.sdz.criminal_intent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import net.sdz.criminal_intent.Crime;
+
+
+import java.util.Date;
+import java.util.UUID;
+import net.sdz.criminal_intent.database.CrimeDbSchema.CrimeTable;
+
+public class CrimeCursorWrapper extends CursorWrapper {
+
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
+    }
+}
