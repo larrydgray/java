12d9dfa0acdda6f182a3213f687f257d2e4852ef
caverdude <javacaverdude@gmail.com>
2021-01-18 17:54:13 -0600

    ch 24 p 472 Nerd Launcher app.

---
+++ android/nerd_launcher/app/src/main/java/net/sdz/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,12 @@
+package net.sdz.nerdlauncher;
+
+
+import androidx.fragment.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
