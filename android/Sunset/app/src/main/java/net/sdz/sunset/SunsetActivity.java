f9a80225c99d7f87c86d56bf2ff6146c24003c22
caverdude <javacaverdude@gmail.com>
2021-01-19 16:21:15 -0600

    ch 32 p620 sunset animation

---
+++ android/Sunset/app/src/main/java/net/sdz/sunset/SunsetActivity.java
@@ -0,0 +1,12 @@
+package net.sdz.sunset;
+
+
+import androidx.fragment.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
