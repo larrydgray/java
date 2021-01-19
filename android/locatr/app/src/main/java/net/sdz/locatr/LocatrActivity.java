e078b9bb7c5ec27d07c921ee333959e07a11275d
caverdude <javacaverdude@gmail.com>
2021-01-19 16:56:02 -0600

    ch 33 p 646 Flicker Image location, finds images cloe to your location.

---
+++ android/locatr/app/src/main/java/net/sdz/locatr/LocatrActivity.java
@@ -0,0 +1,43 @@
+package net.sdz.locatr;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+
+//import com.google.android.gms.common.ConnectionResult;
+//import com.google.android.gms.common.GoogleApiAvailability;
+
+public class LocatrActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
+
+    @Override
+    protected Fragment createFragment() {
+        return LocatrFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = apiAvailability.getErrorDialog(this,
+                    errorCode,
+                    REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            // Leave if services are unavailable.
+                            finish();
+                        }
+                    });
+
+            errorDialog.show();
+        }
+    }
+}
