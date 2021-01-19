0846cbcbe4499452346c408b781f3b00f7f877d2
caverdude <javacaverdude@gmail.com>
2021-01-19 17:51:32 -0600

    Mock walker app that came with chapter 33, neither this code or the app at www.bignerdranch.com/olutions/MockWalker.apk would run.

---
+++ android/mock_walker/app/src/main/java/net/sdz/mockwalker/MockWalkerActivity.java
@@ -0,0 +1,43 @@
+package net.sdz.mockwalker;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+
+//import com.google.android.gms.common.ConnectionResult;
+//import com.google.android.gms.common.GooglePlayServicesUtil;
+
+
+public class MockWalkerActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
+
+    @Override
+    protected Fragment createFragment() {
+        return new MockWalkerFragment();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GooglePlayServicesUtil
+                    .getErrorDialog(errorCode, this, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    // Leave if services are unavailable.
+                                    finish();
+                                }
+                            });
+
+            errorDialog.show();
+        }
+    }
+}
---
+++ android/mock_walker/app/src/main/java/net/sdz/mockwalker/MockWalkerFragment.java
@@ -0,0 +1,75 @@
+package net.sdz.mockwalker;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
+
+//import butterknife.ButterKnife;
+//import butterknife.InjectView;
+//import butterknife.OnClick;
+//import rx.functions.Action1;
+//import rx.subscriptions.CompositeSubscription;
+
+public class MockWalkerFragment extends Fragment {
+    @InjectView(R.id.start_button) CompoundButton mStartButton;
+    private CompositeSubscription mServiceSubscriptions = new CompositeSubscription();
+    private Intent mServiceIntent;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_mockwalker, container, false);
+
+        ButterKnife.inject(this, v);
+
+        mServiceIntent = new Intent(getActivity(), MockWalkerService.class);
+
+        mServiceSubscriptions.add(MockWalker.get(getActivity()).getChanges()
+                .subscribe(new Action1<MockWalker>() {
+                    @Override
+                    public void call(MockWalker mockWalker) {
+                        updateUI();
+                    }
+                }));
+
+        updateUI();
+
+        return v;
+    }
+
+    @OnClick(R.id.start_button)
+    public void onStartButtonClick() {
+        MockWalker mockWalker = MockWalker.get(getActivity());
+
+        if (mockWalker.isStarted()) {
+            getActivity().stopService(mServiceIntent);
+        } else {
+            getActivity().startService(mServiceIntent);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mServiceSubscriptions.unsubscribe();
+    }
+
+    private void updateUI() {
+        MockWalker mockWalker = MockWalker.get(getActivity());
+        if (mockWalker.isStarted()) {
+            mStartButton.setChecked(true);
+        } else {
+            mStartButton.setChecked(false);
+        }
+    }
+}
