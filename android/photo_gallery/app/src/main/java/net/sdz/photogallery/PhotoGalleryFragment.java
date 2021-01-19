2f6f55406ee2006d57a00156117bf4293b58049a
caverdude <javacaverdude@gmail.com>
2021-01-19 01:15:18 -0600

    ch 29 page 580 broadcast intents

--- android/photo_gallery/app/src/main/java/net/sdz/photogallery/PhotoGalleryFragment.java
+++ android/photo_gallery/app/src/main/java/net/sdz/photogallery/PhotoGalleryFragment.java
@@ -16,14 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends VisibleFragment {
     private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
