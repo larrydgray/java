0ff44bedfc45655b42584b7749443d41d88125de
caverdude <javacaverdude@gmail.com>
2021-01-23 20:49:41 -0600

    SpringBoot203

---
+++ spring/SpringBoot_203/src/main/java/net/itjsb/demo/Springboot201Application.java
@@ -0,0 +1,13 @@
+package net.itjsb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot201Application.class, args);
+    }
+
+}
