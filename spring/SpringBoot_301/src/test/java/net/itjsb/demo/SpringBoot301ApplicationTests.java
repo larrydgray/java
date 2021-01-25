977969e04d0dd20015def49939b8ad31b615a46c
caverdude <javacaverdude@gmail.com>
2021-01-24 19:25:28 -0600

    SpringBoot301

---
+++ spring/SpringBoot_301/src/test/java/net/itjsb/demo/SpringBoot301ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.itjsb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
