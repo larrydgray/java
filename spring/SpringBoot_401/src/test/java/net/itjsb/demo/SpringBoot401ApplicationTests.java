93f3f84e8e2ef72a7f9d6a3b5610ff628dd6fa3c
caverdude <javacaverdude@gmail.com>
2021-01-25 16:53:09 -0600

    SpringBoot 401

---
+++ spring/SpringBoot_401/src/test/java/net/itjsb/demo/SpringBoot401ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.itjsb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
