b6d3bdacb73237ab777d8ae073f18e612bdb72a7
caverdude <javacaverdude@gmail.com>
2021-01-26 13:38:36 -0600

    SpringBoot 501

---
+++ spring/SpringBoot_501/src/test/java/net/itjsb/demo/SpringBoot501ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.itjsb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot501ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
