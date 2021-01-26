d75421c48214927363c166465a0bf24d64041af4
caverdude <javacaverdude@gmail.com>
2021-01-25 22:51:42 -0600

    SpringBoot 406

---
+++ spring/SpringBoot_406/src/main/java/net/itjsb/demo/Springboot406Application.java
@@ -0,0 +1,13 @@
+package net.itjsb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot406Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot406Application.class, args);
+  }
+
+}
